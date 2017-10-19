6d32a5ab880e649da3b0034891d0dea29080c711
Tom McAdam <tmcadam@users.noreply.github.com>
2017-10-19 15:02:14 +0900

    Basic Python build environment

---
+++ build.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Go to the project directory
+PROJECT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd ${PROJECT_DIR}
+
+# Setup virtualenv, if it doesn't already exist
+if [ -d ".env" ]; then
+    echo "**> virtualenv exists"
+else
+    echo "**> creating virtualenv"
+    pyvenv-3.5 .env
+fi
+
+# Enter virtualenv
+set +u
+source .env/bin/activate
+set -u
+
+# Install dependencies
+pip install -U pip-tools
+pip install -U setuptools
+pip-compile -o requirements.txt requirements.in > /dev/null
+pip-compile -o requirements-dev.txt requirements-dev.in > /dev/null
+pip-sync requirements.txt requirements-dev.txt
+
+# Run tests
