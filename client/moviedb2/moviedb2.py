a0c7899e08d7cc48faa71c23f47dd274f7f943d4
Tom McAdam <tmcadam@users.noreply.github.com>
2017-10-20 12:29:53 +0900

    Added a few funcs and tests to find files

--- client/moviedb2/moviedb2.py
+++ client/moviedb2/moviedb2.py
@@ -1,4 +1,19 @@
 import requests
+import os
+import re
+
+def get_files_without_id( files ):
+    return [ f for f in files if not re.search('\{tt\d{7}\}', f) ]
+
+def get_files( path ):
+    FILE_EXTS = [".avi", ".mp4", ".mkv"]
+    return [os.path.join(root, name)
+            for root, dirs, files in os.walk(path)
+            for name in files
+            if os.path.splitext(name)[1] in FILE_EXTS]
+
+def check_folder_exists( path ):
+    return os.path.exists(path) and os.path.isdir(path)
 
 def hello_world():
     r = requests.get('https://www.google.com.au')
--- client/tests/test-moviedb2.py
+++ client/tests/test-moviedb2.py
@@ -1,8 +1,63 @@
-from moviedb2 import moviedb2
+import os
 import unittest
+import shutil
+from pathlib import Path
+from nose.tools import assert_equals
 
+from moviedb2 import moviedb2
 
 class Moviedb2TestCase(unittest.TestCase):
 
+    def test_get_files_without_id_returns_subset_of_files(self):
+        Path(os.path.join(self.test_folder, "blah {1980}.avi")).touch() # this one has an invalid IMDB id + 1 in the setUp = 2
+        files = moviedb2.get_files_without_id(moviedb2.get_files(self.test_folder))
+        assert_equals(len(files), 2)
+
+    def test_get_files_returns_all_movie_files(self):
+        files = moviedb2.get_files(self.test_folder)
+        assert len(files) == 3
+        for f in self.files:
+            assert f in files
+
+    def test_get_files_excludes_non_movie_files(self):
+        Path(os.path.join(self.test_folder, "test1.sub")).touch()
+        files = moviedb2.get_files(self.test_folder)
+        assert_equals(len(files), 3)
+
+    def test_get_files_returns_none_if_empty(self):
+        shutil.rmtree(self.test_folder)
+        os.mkdir(self.test_folder)
+        assert not moviedb2.get_files(self.test_folder)
+
+    def test_check_folder_exists_returns_true_if_folder_exists(self):
+        assert moviedb2.check_folder_exists(self.test_folder)
+
+    def test_check_folder_exists_returns_false_if_no_folder(self):
+        shutil.rmtree(self.test_folder)
+        assert not moviedb2.check_folder_exists(self.test_folder)
+
+    def test_check_folder_exists_return_false_if_file_not_folder(self):
+        shutil.rmtree(self.test_folder)
+        Path(self.test_folder).touch()
+        assert not moviedb2.check_folder_exists(self.test_folder)
+
     def test_hello_world(self):
-        assert moviedb2.hello_world() == 200
+        assert_equals(moviedb2.hello_world(), 200)
+
+    def setUp(self):
+        self.test_folder = os.path.join("tests", "test_movies")
+        self.files = [os.path.join(self.test_folder, "test1 {tt1234567}.avi"),
+                        os.path.join(self.test_folder, "A", "test2 {tt2345678}.mp4"),
+                        os.path.join(self.test_folder, "B", "test3.mkv")]
+        os.mkdir(self.test_folder)
+        os.mkdir(os.path.join(self.test_folder, "A"))
+        os.mkdir(os.path.join(self.test_folder, "B"))
+        for f in self.files:
+            Path(f).touch()
+
+    def tearDown(self):
+        if os.path.exists(self.test_folder):
+            if os.path.isdir(self.test_folder):
+                shutil.rmtree(self.test_folder)
+            else:
+                os.remove(self.test_folder)
