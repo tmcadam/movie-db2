c64f2f82bf75f3ea41d09f4429ba887285c30739
Tom McAdam <tmcadam@users.noreply.github.com>
2017-10-21 23:53:10 +0900

    Server: Added a POST movie endpoint

--- server/moviedb2/moviedb2.py
+++ server/moviedb2/moviedb2.py
@@ -1,7 +1,15 @@
-from flask import Flask
+import json
+from flask import Flask, request
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
 
+@app.route('/moviesdb2/api/v1.0/filenames', methods=['POST'])
+def get_tasks():
+    print (request.is_json)
+    content = request.get_json()
+    print (content)
+    return json.dumps({'success':True}), 200, {'ContentType':'application/json'}
--- server/tests/test-moviedb2.py
+++ server/tests/test-moviedb2.py
@@ -1,6 +1,6 @@
 import moviedb2
 import unittest
-
+import json
 
 class Moviedb2TestCase(unittest.TestCase):
 
@@ -11,3 +11,10 @@ class Moviedb2TestCase(unittest.TestCase):
         rv = self.app.get('/')
         assert rv.status_code == 200
         assert b'Hello, World!' in rv.data
+
+    def test_can_post_new_filename(self):
+        data = {"filename": "somefilename"}
+        rv = self.app.post('/moviesdb2/api/v1.0/filenames',
+                           data=json.dumps(data),
+                           content_type='application/json')
+        assert rv.status_code == 200
